e170620e8b7005608c08133d0583372f09c484a4
ignaciogironas0431 <ignacio.gironas@ucb.edu.bo>
2026-06-15 15:36:51 -0400

    Create utilidades.h

---
+++ include/utilidades.h
@@ -0,0 +1,10 @@
+#ifndef UTILIDADES_H
+#define UTILIDADES_H
+
+#include "AnimaticFlow.h"
+
+// Manejo estricto de la interfaz de usuario en la terminal
+void limpiarPantallaNativa();
+void detenerFlujo();
+
+#endif
