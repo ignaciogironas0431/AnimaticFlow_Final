d4cc1f290219ed8d386e4b740888ff52342be9e4
ignaciogironas0431 <ignacio.gironas@ucb.edu.bo>
2026-06-18 23:24:22 -0400

    Update logica.h

--- include/logica.h
+++ include/logica.h
@@ -9,11 +9,11 @@ void visualizarTablero(const vector<Escena>& lista);
 void modificarEstadoEscena(vector<Escena>& lista);
 void eliminarEscenaFisica(vector<Escena>& lista);
 
-// Métricas solicitadas y sugeridas para el Grupo 1
+// Métricas solicitadas y sugeridas 
 void ejecutarModuloComparativo(vector<Escena>& lista);
 void ordenarBubbleSortConMetricas(vector<Escena>& lista, int& comparaciones, int& intercambios);
 
-// Funciones de búsqueda exigidas por la rúbrica (Iterativa y Recursiva)
+// Funciones de búsqueda (Iterativa y Recursiva)
 int buscarBinariaIterativa(const vector<Escena>& lista, const string& idBuscado, int& iteraciones);
 int buscarBinariaRecursiva(const vector<Escena>& lista, int izquierda, int derecha, const string& idBuscado, int& llamadas);
 
